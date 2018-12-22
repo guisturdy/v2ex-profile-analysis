5f4089a313e48cc1295047bd6f7995509e187ff5
guisturdy <guistrudy@live.com>
2018-12-23 01:22:26 +0800

    perf(spider): optimised

--- spider/index.js
+++ spider/index.js
@@ -7,10 +7,88 @@ const db = new sqlite3.Database('v2ex.db')
 
 const CONFIG = require('./config').config
 
-const porxy = [
+const proxy = [
   ...(CONFIG.proxy || []).map(proxy => new HttpsProxyAgent(proxy)),
   null
 ]
+
+const freeProxy = 'x'.repeat(proxy.length).split('').map((_, i) => i)
+const works = []
+let workInterval
+let requestCount = 0, warningCount = 0, errorCount = 0
+
+let needFetchID = CONFIG.startID
+
+function addWork(id, page = 1) {
+  needFetchID = Math.max(id + 1, needFetchID)
+  works.push([id, page])
+}
+
+function finishWork(id, page) {
+  inWork.delete(`${id}:${page}`)
+}
+
+function getWork() {
+  if (works.length === 0) {
+    addWork(needFetchID)
+    if (needFetchID > CONFIG.endID) {
+      clearInterval(workInterval)
+      return
+    }
+  }
+  return works.pop()
+}
+
+async function doFetch(proxyIndex, work) {
+  if (!work) {
+    return
+  }
+  const [id, page] = work
+  const url = `https://www.v2ex.com/amp/t/${id}/${page}`
+  const startDate = new Date()
+  await Promise.all([
+    fetch(url, { agent: proxy[proxyIndex] })
+      .then(res => res.text())
+      .then(body => {
+        logContent(body)
+        insertRow({
+          id, page, info: body, status: 200
+        })
+        const _page = body.match(/class=['"]pagination['"].*?共([0-9\s]*?)页/) || [0, 0]
+        const totalPage = parseInt(_page[1]) || 0
+        if (!totalPage) {
+          warningCount++
+        } else if (+totalPage > 1 && page === 1) {
+          for (let p = totalPage; p > 1; p--) {
+            addWork(id, p)
+          }
+        }
+        console.log(colors.gray(`page: ${page}/${totalPage}`))
+      }).catch(e => {
+        errorCount++
+        addWork(id, page)
+        console.log(e)
+      }).then(() => {
+        const endDate = new Date()
+        const cost = endDate.getTime() - startDate.getTime()
+        requestCount++
+        logInfo({ url, cost, requestCount, warningCount, errorCount, proxyIndex })
+      }),
+    new Promise(r => setTimeout(r, 1000 * 5))
+  ]).then(() => {
+    freeProxy.push(proxyIndex)
+  })
+}
+
+function startWork() {
+  workInterval = setInterval(() => {
+    if (freeProxy.length) {
+      doFetch(freeProxy.pop(), getWork())
+    } else {
+      console.log(colors.gray(`all proxy is basy`))
+    }
+  }, ~~(1000 * 5 / proxy.length));
+}
 
 const insertRow = ({ id, page, info, status }) => {
   db.run("INSERT INTO v2ex (id, page, info, status) VALUES (?, ?, ?, ?)", id, page, info, status)
@@ -23,49 +101,9 @@ db.serialize(function () {
     }
   })
 
-  run()
+  startWork()
 
 })
-
-async function run() {
-  console.log('start')
-  let requestCount = 0, errorCount = 0, warningCount = 0
-  for (let index = CONFIG.startID; index < CONFIG.endID; index++) {
-    let totalPage = 1, nowPage = 1
-    while (totalPage >= nowPage) {
-      const url = `https://www.v2ex.com/amp/t/${index}/${nowPage}`
-      const startDate = new Date()
-      await Promise.all([
-        fetch(url, { agent: porxy[requestCount % porxy.length] })
-          .then(res => res.text())
-          .then(body => {
-            logContent(body)
-            insertRow({
-              id: index, page: nowPage++, info: body, status: 200
-            })
-            const page = body.match(/class=['"]pagination['"].*?共([0-9\s]*?)页/) || [0, 0]
-            totalPage = parseInt(page[1]) || 0
-            if (!totalPage) {
-              warningCount++
-            }
-            console.log(colors.gray(`page: ${nowPage - 1}/${totalPage}`))
-          }).catch(e => {
-            insertRow({
-              id: index, page: nowPage++, info: e, status: 0
-            })
-            errorCount++
-            console.log(e)
-          }).then(() => {
-            const endDate = new Date()
-            const cost = endDate.getTime() - startDate.getTime()
-            requestCount++
-            logInfo({ url, cost, requestCount, warningCount, errorCount, proxyIndex: requestCount % porxy.length })
-          }),
-        new Promise(r => setTimeout(r, 1000 * 5 / porxy.length))
-      ])
-    }
-  }
-}
 
 function logContent(body) {
   const logText = body.match(/ class="topic_title"([\s\S]*?)class="topic_content"/)
